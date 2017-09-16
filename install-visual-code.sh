a0c61eaab9f88d8e8577c6d6c4b8f1c2a7cb9803
Leo <leonhard.radonic@gmail.com>
2017-09-16 16:38:49 +0200

    Install visual-code commands

---
+++ install-visual-code.sh
@@ -0,0 +1,6 @@
+
+## add repository and install visual-code
+sudo add-apt-repository -y "deb https://packages.microsoft.com/repos/vscode stable main"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
+sudo apt update
+sudo apt -y install code
