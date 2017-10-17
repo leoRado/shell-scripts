96b85a50c474040d58888c0e1c24a8d4ab3c16aa
NerdOfLinux <nerdoflinux@gateblogs.com>
2017-10-17 16:41:49 -0500

    Update install-java.sh
    Added auto-check for root to prevent using sudo in script. Also, it now tries to use apt which is newer, but falls back to apt-get.

--- install-java.sh
+++ install-java.sh
@@ -1,5 +1,24 @@
-
-sudo apt-get install openjdk-8-jdk
-apt-cache search jdk
+#Check for root
+if [ $(whoami) != "root" ]
+then
+  echo "Please run as root."
+  exit
+fi
+#If user is root, continue
+#Check for apt, with a fallback to apt-get
+if [ $(which apt) ]
+then
+  APT="apt"
+elif [ $(which apt-get) ]
+then
+  APT="apt-get"
+else
+  echo "Warning, apt and apt-get were not found on your system."
+  exit
+fi
+#Install
+$APT install openjdk-8-jdk
+APT search jdk
+#Configure paths
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
 export PATH=$PATH:/usr/lib/jvm/java-8-openjdk/bin
